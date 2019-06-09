5079aa7f2652bd41393459c88e880b59f0fe8a2b
Cristina <cristinachen1204@gmail.com>
2019-06-09 09:31:49 -0700

    basic login with google

---
+++ lib/googleOauth2.ts
@@ -0,0 +1,7 @@
+class googleOauth2 {
+    
+static id: string = '76636354965-414pk5pb8ucrp297ve99spppl9o8pm1k.apps.googleusercontent.com';
+static secret:string = '2lSOybUafnMGSErXkDuScZ_p';
+
+}
+export default googleOauth2;
